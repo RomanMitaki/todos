dbab45afc80fee4ea588b42981ab6cdf204f8ae4
RomanMitaki <r.mitaki@yandex.ru>
2023-09-08 21:47:08 +0300

    add css-modules, MiniCssExtractPlugin

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Counter} from "./Counter";
+import './index.css';
+
+const App = () => {
+    return (
+        <div className='app'>
+            TEST
+            <Counter />
+        </div>
+    );
+};
+
+export default App;
