7ea7efcf0f9f50564bc7dfdfc02c037a66570fc2
RomanMitaki <r.mitaki@yandex.ru>
2023-09-09 14:09:07 +0300

    add classNames fn

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,6 +2,7 @@ import React  from 'react';
 import {Counter} from "./Counter";
 import '../styles/index.css';
 import {useTheme} from "../assets/theme/useTheme";
+import {classNames} from "../assets/classNames/classNames";
 
 
 
@@ -9,7 +10,7 @@ const App = () => {
     const {theme, toggleTheme} = useTheme()
 
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames('app', {}, [theme])}>
             TEST
             <Counter />
             <button onClick={toggleTheme}>THEME</button>
