b7467449c2111f91e9e61e6d93a152d5978131e2
RomanMitaki <r.mitaki@yandex.ru>
2023-09-09 19:35:18 +0300

    make jest-config

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -4,6 +4,7 @@
  */
 
 import type { Config } from 'jest'
+import path from 'path'
 
 const config: Config = {
   // All imported modules in your tests should be mocked automatically
@@ -35,7 +36,12 @@ const config: Config = {
   testMatch: [
     '<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)'
   ],
-  rootDir: '../../'
+  rootDir: '../../',
+  setupFilesAfterEnv: ['<rootDir>config/jest/setupTests.ts'],
+  moduleNameMapper: {
+    '\\.(css|less)$': 'identity-obj-proxy',
+    '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx')
+  }
 
   // Indicates whether the coverage information should be collected while executing the test
   // collectCoverage: false,
